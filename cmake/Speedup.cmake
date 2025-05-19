8aaa1189d97bbe25282a00ff9e5df20458d45377
Jonas Heinle <jonasheinle@googlemail.com>
2025-05-19 10:05:28 +0200

    Update Speedup.cmake

--- cmake/Speedup.cmake
+++ cmake/Speedup.cmake
@@ -1,6 +1,7 @@
 # https://cmake.org/cmake/help/latest/module/ProcessorCount.html
 
 include(ProcessorCount)
+# 1. Detect number of cores
 ProcessorCount(N)
 message(STATUS "Processor count: ${N}")         # prints “-- Processor count: <N>” during configure
 if(NOT
@@ -9,5 +10,6 @@ if(NOT
    0)
   set(CTEST_BUILD_FLAGS -j${N})
   set(ctest_test_args ${ctest_test_args} PARALLEL_LEVEL ${N})
-  set(CMAKE_BUILD_PARALLEL_LEVEL ${N})
+  # 2. Export as environment variable
+  set(ENV{CMAKE_BUILD_PARALLEL_LEVEL} "${N}")
 endif()
