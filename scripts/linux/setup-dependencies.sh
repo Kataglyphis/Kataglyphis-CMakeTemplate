6f1c1fbc1c5bd58b82dba824bf3d9deb0cc498ae
Jonas Heinle <jonasheinle@googlemail.com>
2025-09-30 19:19:51 +0200

    Update setup-dependencies.sh

--- scripts/linux/setup-dependencies.sh
+++ scripts/linux/setup-dependencies.sh
@@ -76,6 +76,12 @@ if command -v apt-get >/dev/null; then
       sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-"${CLANG_WANTED}" 100
     fi
 
+    # llvm-profdata
+    if [ -x "/usr/bin/llvm-profdata-${CLANG_WANTED}" ]; then
+      sudo update-alternatives --install /usr/bin/llvm-profdata llvm-profdata /usr/bin/llvm-profdata-"${CLANG_WANTED}" 100
+      sudo update-alternatives --set llvm-profdata /usr/bin/llvm-profdata-"${CLANG_WANTED}"
+    fi
+    
     # Verify
     clang --version
     clang++ --version
